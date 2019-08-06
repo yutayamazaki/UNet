58b3e4b2832876024b536bb44d2e77fb97da42ae
yutayamazaki <tppymd@gmail.com>
2019-08-07 08:49:44 +0900

    add scheduler

--- train.py
+++ train.py
@@ -67,9 +67,12 @@ def load_data():
 
 
 if __name__ == '__main__':
-    batch_size = 2
-    num_epochs = 50
+    batch_size = 9
+    num_epochs = 200
     num_classes = 21
+    min_lr = 1e-4
+    max_lr = 0.1
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     model = UNet(in_channels=3, num_classes=21)
@@ -89,9 +92,13 @@ if __name__ == '__main__':
     valid_loader = torch.utils.data.DataLoader(dvalid, batch_size=batch_size)
 
     optimizer = torch.optim.SGD(model.parameters(),
-                                lr=0.01,
+                                lr=max_lr,
                                 momentum=0.9,
                                 weight_decay=3e-4)
+
+     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                            T_max=num_epochs,
+                                                            eta_min=min_lr)
 
     trainer = SegmentationTrainer(model, optimizer, criterion, num_classes=num_classes)
 
@@ -99,7 +106,10 @@ if __name__ == '__main__':
         train_loss = trainer.epoch_train(train_loader)
         valid_loss = trainer.epoch_eval(valid_loader)
 
+        scheduler.step()
+
         print(f'EPOCH: [{epoch}/{num_epochs}]')
         print(f'TRAIN LOSS: {train_loss:.3f}, VALID LOSS: {valid_loss:.3f}')
-        # print(f'TRAIN IOU: {train_iou:.3f}, VALID IOU: {valid_iou:.3f}')
-        torch.save(trainer.weights, f'epoch_{epoch}_loss_{valid_loss:.3f}.pth')
+
+        path = os.path.join('weights', f'epoch{epoch}_loss{valid_loss:.3f}.pth')
+        torch.save(trainer.weights, path)
